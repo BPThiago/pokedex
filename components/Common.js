f22cfc3ff1ab1c6c35f8b7ce449c5259b98e6141
BPThiago <thiagoborges980@gmail.com>
2022-05-26 12:23:33 -0300

    Add Head

---
+++ components/Common.js
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+export default function Commmon({children}) {
+    return (
+        <>
+        <Head>
+            <title>Pokédex</title>
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <meta name="description" content="A simple next.js pokedex from PokéApi data." />
+            <meta
+                name="keywords"
+                content="pokedex, pokemon, next.js, react, api, pokeapi, javascript"
+            />
+
+
+        </Head>
+        <div className={styles.initialContainer}>
+        
+            <header className={styles.navBar}>
+                <Link href={'/'}>
+                    <div className={styles.logo}>
+
+                    </div>
+                </Link>
+                <Link href={'/'}>
+                    <p className={styles.redirectHome}>Home</p>
+                </Link>
+            </header>
+            {children}
+        </div>
+        </>
+    )
+}
