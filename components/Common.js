5590ca06f97c3d11c8e9657ad950e7400c9c2f14
BPThiago <75534648+BPThiago@users.noreply.github.com>
2022-06-15 18:41:58 -0300

    Update Common.js
    Fix Commmon to Common

--- components/Common.js
+++ components/Common.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-export default function Commmon({children}) {
+export default function Common({children}) {
     return (
         <>
         <Head>
